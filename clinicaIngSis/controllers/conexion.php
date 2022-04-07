c6c3a06bfbb05606571fe2830c7a6db819c64752
Johan Carrillo <jcarrillob3@miumg.edu.gt>
2022-04-06 22:05:53 -0600

    [DEV] Feat Se ha agregado el inicio del codigo fuente

---
+++ clinicaIngSis/controllers/conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+	$db_host = "localhost";                              //HOST
+	$db_user = "root";						             //USER
+	$db_pass = "";										 //PASSWORD
+	$db_nombre = "dbclinicaadmin";                       //nombre DB
+	$port = "3306";
+
+?>
---
+++ clinicaIngSis/tienda/conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+	$db_host = "localhost";
+	$db_user = "id17547686_clinicaproyecto";
+	$db_pass = "Proyectodb.2021";
+	$db_nombre = "id17547686_db_clinicaproyecto_2021";
+	$port = "3306";
+
+?>
